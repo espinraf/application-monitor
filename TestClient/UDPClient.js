55a345121baab39884defddabcf8dca39835b5c5
Rafael Espino <rafael.espino@tele2.com>
2015-06-02 16:06:58 +0200

    Added JSON

--- TestClient/UDPClient.js
+++ TestClient/UDPClient.js
@@ -1,8 +1,24 @@
-var PORT = 33333;
+var PORT = 9090;
 var HOST = '127.0.0.1';
 
 var dgram = require('dgram');
-var message = new Buffer('My KungFu is Good!');
+
+data = '{ ' +
+'"Id" : "i00${i + 1}", ' +
+'"Name" : "Customer", ' +
+'"Type" : "JMS", ' +
+'"Status" : "OK", ' +
+'"data": ' +
+'[ ' +
+'{ "name" : "msgRecv", "value" : "1", "type" : "T" }, ' +
+'{ "name": "msgSend", "value" : "5", "type" : "C" }, ' +
+'{ "name": "Errors", "value" : "13", "type" : "V" }, ' +
+'{ "name": "ErrorXSLT", "value" : "true", "type" : "B" }, ' +
+'{ "name": "LastError", "value" : "NO ERRORS", "type" : "S" } ' +
+'] ' +
+'}';
+
+var message = new Buffer(data);
 
 var client = dgram.createSocket('udp4');
 client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
