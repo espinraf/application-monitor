60ba18b6d827717492990bad22101590716d7311
Rafael Espino <rafael.espino@redpill-linpro.com>
2015-06-05 14:57:52 +0200

    Updated Nodejs Client

--- TestClient/UDPClient.js
+++ TestClient/UDPClient.js
@@ -2,27 +2,41 @@ var PORT = 9090;
 var HOST = '127.0.0.1';
 
 var dgram = require('dgram');
+var readline = require('readline');
 
 data = '{ ' +
-'"Id" : "i00${i + 1}", ' +
-'"Name" : "Customer", ' +
-'"Type" : "JMS", ' +
+'"Id" : "i00JS", ' +
+'"Name" : "Nodejs", ' +
+'"Type" : "JS", ' +
 '"Status" : "OK", ' +
 '"data": ' +
 '[ ' +
-'{ "name" : "msgRecv", "value" : "1", "type" : "T" }, ' +
-'{ "name": "msgSend", "value" : "5", "type" : "C" }, ' +
+'{ "name" : "Requests", "value" : "1", "type" : "T" }, ' +
+'{ "name": "Response", "value" : "5", "type" : "C" }, ' +
 '{ "name": "Errors", "value" : "13", "type" : "V" }, ' +
-'{ "name": "ErrorXSLT", "value" : "true", "type" : "B" }, ' +
+'{ "name": "ErrorHTTP", "value" : "true", "type" : "B" }, ' +
 '{ "name": "LastError", "value" : "NO ERRORS", "type" : "S" } ' +
 '] ' +
 '}';
 
 var message = new Buffer(data);
 
-var client = dgram.createSocket('udp4');
+
+var client;
+
+var rl = readline.createInterface(process.stdin, process.stdout);
+rl.setPrompt('$ ');
+rl.prompt();
+
+rl.on('line', function(line) {
+    if (line === "exit") rl.close();
+client = dgram.createSocket('udp4');
 client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
     if (err) throw err;
     console.log('UDP message sent to ' + HOST +':'+ PORT);
     client.close();
+});
+    rl.prompt();
+}).on('close',function(){
+    process.exit(0);
 });
