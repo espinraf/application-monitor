b8963f78964110afb3a589889984c6a577a8b8ac
Rafael Espino <rafael.espino@redpill-linpro.com>
2015-06-14 17:17:47 +0200

    Added TCP Support

---
+++ src/main/java/se/voipbusiness/core/MonitorTCPServer.java
@@ -0,0 +1,57 @@
+package se.voipbusiness.core;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Created by espinraf on 14/06/15.
+ */
+public class MonitorTCPServer {
+
+    public Monitor mon = null;
+    public int tcpPort = Integer.valueOf(System.getProperty("tcpServer.port", "9095"));
+    public ServerSocket tcpSocket = null;
+    public String data;
+
+    public void init(){
+        try {
+            tcpSocket = new ServerSocket(tcpPort);
+            System.out.println("TCP Server started on port: " + tcpPort);
+        }
+        catch (IOException ex) {
+            if (mon != null) {
+                mon.logMsg("UDP Server Error: \n");
+                mon.logMsg(ex.getMessage());
+            }
+            else {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    public void run(){
+
+        while(true)
+        {
+            try {
+                Socket connectionSocket = tcpSocket.accept();
+                BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+
+                data = inFromClient.readLine();
+            }
+            catch (IOException ex) {
+                if (mon != null) {
+                    mon.logMsg("UDP Server Error: \n");
+                    mon.logMsg(ex.getMessage());
+                }
+                else {
+                    ex.printStackTrace();
+                }
+            }
+
+        }
+    }
+}
